56ee91de3ed1a823be21c9920b07ed55ef87cdad
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2021-09-22 04:31:13 +0530

    add sign in page

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -1,1 +1,10 @@
-export const routes = [];
+import SignInPage from '../pages/SignInPage';
+
+export const routes = [
+  {
+    path: '/sign-in',
+    Component: SignInPage,
+    private: false,
+    exact: true,
+  },
+];
